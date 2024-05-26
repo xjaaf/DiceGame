17f934d1be2f593be56038658957905addd532c2
JAF <xjaaf@icloud.com>
2024-05-26 13:10:53 +0200

    pruebas test

--- DiceGame/tests/Feature/PlayerTest.php
+++ DiceGame/tests/Feature/PlayerTest.php
@@ -9,22 +9,13 @@ use App\Http\Controllers\GameController;
 use Tests\TestCase;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Artisan;
 use App\Models\Game;
 use Mockery\Generator\StringManipulation\Pass\Pass;
 
 class PlayerTest extends TestCase
 {
 
-
-    use RefreshDatabase, WithFaker;
-
-    public function setUp(): void
-    {
-        parent::setUp();
-        $this->artisan('db:seed');
-        $this->artisan('passport:install');
-    }
-    
     public function test_register()
     {
         
@@ -48,4 +39,34 @@ class PlayerTest extends TestCase
         ]);
     }
 
+    public function test_login()
+    {
+        $response = $this->postJson('/api/players/login', [
+            'email' => 'test@example.com',
+            'password' => 'password',
+            
+        
+        ]);
+
+        $response->assertStatus(200);
+        
+    }
+
+    public function test_update()
+    {
+        $response = $this->postJson('/api/players/login', [
+            'email' => 'test@example.com',
+            'password' => 'password',
+            
+        ]);
+
+        $response = $this->putJson('/api/players/' . $response['user']['id'], [
+            'Bearer ' => $response['access_token'],
+            'name' => 'Updated Name',
+            'nickname' => 'updatednickname',
+        ]);
+
+        $response->assertStatus(200);
+
+    }
 }
